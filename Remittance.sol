1990d8801aee67ed5d68d6bd11d9e5b5da0e0152
Nate Wickstrom <nwickstrom@pandora.com>
2018-07-01 16:31:10 -0700

    make Remitter a generic until, not a one-off

--- Remittance.sol
+++ Remittance.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.24;
 
+
 /**
  * @title Remittance
  *
@@ -8,57 +9,61 @@ pragma solidity ^0.4.24;
  */
 contract Remittance {
 
-    address owner;
-    address receiver;
-    bytes32 passcode;
+    struct Account {
+        uint balance;
+        bytes32 passcode;
+    }
 
-    uint funds;
+    mapping(address => Account) public accounts;
 
     event LogDeposit(address from, uint funds);
-    event LogTransfer(address to, uint funds);
+    event LogWithdrawl(address to, uint funds);
 
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only the owner can do this"); _;
-    }
-
-    modifier onlyReceiver {
-        require(msg.sender == receiver, "Only the receiver can do this"); _;
-    }
-
-    constructor(address to, bytes32 code) public {
+    /**
+     * @dev Deposit funds into this contract that can only be withdrawn by the
+     * give 'to' address and they must supply.
+     *
+     * @param to whom to transfer funds to.
+     * @param code to protect the given funds.
+     */
+    function deposit(address to, bytes32 code) public payable {
+        require(msg.value > 0, "Insufficient funds");
         require(to != address(0), "To Address must not be 0");
         require(code != bytes32(0), "Passcode is not strong enough");
+        require(hasPendingFunds(to), "Only one pending transfer per address is allowed at a time");
 
-        owner = msg.sender;
-        receiver = to;
-        passcode = code;
-    }
-
-    function send() public payable onlyOwner {
-        require(msg.value > 0, "Insufficient funds");
-
-        funds = msg.value;
-
+        // create a new Account to store the deposit
+        accounts[to] = Account(msg.value, code);
         emit LogDeposit(msg.sender, msg.value);
     }
 
-    function receive(bytes8 exchangePassword, bytes8 recipientPassword) public onlyReceiver returns (bool success) {
-         require(funds > 0, "Insufficient funds");
+    /**
+    * @dev withdrawl funds for the senders accound, provided the supply the
+    * two correct passwords.
+    *
+    * @param exchangePassword intermediary password.
+    * @param recipientPassword final recipient password.
+    */
+    function withdrawl(bytes8 exchangePassword, bytes8 recipientPassword) public {
+        require(hasPendingFunds(msg.sender), "No pending transfers");
+        // todo prevent bruteforce attacks by locking accounts after a certain number is tries
+        require(accounts[msg.sender].passcode == sha256(exchangePassword, recipientPassword),
+                "Incorrect passcodes");
+        
+        uint funds = accounts[msg.sender].balance;
+        // update state before sending funds to prevent reentry attacks
+        accounts[msg.sender] = Account(0,0);
 
-        if (passcode != sha256(exchangePassword, recipientPassword)) {
-            // Don't revert here because we want to make it expesive
-            // to submit wrong passwords. Just log the failure and return early.
-            return false;
+        if (msg.sender.send(funds)) {
+            emit LogWithdrawl(msg.sender, funds);
+            // todo notifiy depositer transfer complete
+        } else {
+            revert("Failed to send funds");
         }
+    }
 
-        if (receiver.send(funds)) {
-            funds = 0;
-            emit LogTransfer(receiver, funds);
-            // todo notifiy owner
-            return true;
-        }
-
-        revert("Failed to send funds");
+    function hasPendingFunds(address to) private view returns (bool) {
+        return accounts[msg.sender].balance > 0;
     }
 
 }
