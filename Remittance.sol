2812d40e81c89351a691f1f5e46e1b534818f2eb
Nate Wickstrom <nwickstrom@pandora.com>
2018-07-04 10:35:09 -0700

    allow multiple pending transfers

--- Remittance.sol
+++ Remittance.sol
@@ -11,59 +11,57 @@ contract Remittance {
 
     struct Account {
         uint balance;
-        bytes32 passcode;
+        address payee;
+        bytes32 password;
     }
 
-    mapping(address => Account) public accounts;
+    mapping(uint256 => Account) public accounts;
 
-    event LogDeposit(address from, address to, uint funds);
-    event LogWithdrawl(address to, uint funds);
+    event LogDeposit( uint accountId, address from, address to, uint funds);
+    event LogWithdraw(uint accountId, address to, uint funds);
 
     /**
      * @dev Deposit funds into this contract that can only be withdrawn by the
      * give 'to' address and they must supply.
      *
-     * @param to whom to transfer funds to.
-     * @param code to protect the given funds.
+     * @param accountId used to id the transfer.
+     * @param payee to transfer funds to.
+     * @param password to protect the given funds.
      */
-    function deposit(address to, bytes32 code) public payable {
+    function deposit(uint accountId, address payee, bytes32 password) public payable {
         require(msg.value > 0, "Insufficient funds");
-        require(to != address(0), "To Address must not be 0");
-        require(code != bytes32(0), "Passcode is not strong enough");
-        require(hasPendingFunds(to), "Only one pending transfer per address is allowed at a time");
+        require(payee != address(0), "To Address must not be 0");
+        require(password != bytes32(0), "Password is not strong enough");
+        require(accounts[accountId].password != bytes32(0), "Account in use");
 
         // create a new Account to store the deposit
-        accounts[to] = Account(msg.value, code);
-        emit LogDeposit(msg.sender, to, msg.value);
+        accounts[accountId] = Account(msg.value, payee, password);
+        emit LogDeposit(accountId, msg.sender, payee, msg.value);
     }
 
     /**
     * @dev withdrawl funds for the senders accound, provided the supply the
     * two correct passwords.
     *
+    * @param accountId used to id the transfer.
     * @param exchangePassword intermediary password.
     * @param recipientPassword final recipient password.
     */
-    function withdrawl(string exchangePassword, string recipientPassword) public {
-        require(hasPendingFunds(msg.sender), "No pending transfers");
-        // todo prevent bruteforce attacks by locking accounts after a certain number is tries
-        require(accounts[msg.sender].passcode == sha256(exchangePassword, recipientPassword),
-                "Incorrect passcodes");
+    function withdraw(uint accountId, string exchangePassword, string recipientPassword) public {
+        uint balance = accounts[accountId].balance;
+        bytes32 password = accounts[accountId].password;
+        address payee = accounts[accountId].payee;
 
-        uint funds = accounts[msg.sender].balance;
+        require(msg.sender == payee, "Only the payee can do this");
+        require(balance > 0, "No pending transfers");
+        require(password == sha256(exchangePassword, recipientPassword), "Incorrect passwords");
+
         // update state before sending funds to prevent reentry attacks
-        accounts[msg.sender] = Account(0,0);
+        delete accounts[accountId];
 
-        if (msg.sender.send(funds)) {
-            emit LogWithdrawl(msg.sender, funds);
-            // todo notifiy depositer transfer complete
-        } else {
-            revert("Failed to send funds");
-        }
-    }
-
-    function hasPendingFunds(address to) private view returns (bool) {
-        return accounts[to].balance > 0;
+        // make the transfer
+        require(msg.sender.send(balance), "Failed to send funds");
+        emit LogWithdraw(accountId, msg.sender, balance);
     }
 
 }
