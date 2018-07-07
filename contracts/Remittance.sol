6def3bcdbe1f8b3aaacf53edd18d3391bfe0b190
nate <nate@example.com>
2018-07-07 12:52:23 -0700

    added tests

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -33,7 +33,7 @@ contract Remittance {
         require(msg.value > 0, "Insufficient funds");
         require(msg.value <= MAX_AMOUNT, "Funds exceed transaction limit");
         require(password != bytes32(0), "Password is not strong enough");
-        require(accounts[accountId].password != bytes32(0), "Account in use");
+        require(accounts[accountId].password == bytes32(0), "Account in use");
         require(!usedPasswords[password], "Cannot reuse passwords");
 
         // create a new Account to store the deposit
@@ -55,7 +55,8 @@ contract Remittance {
         bytes32 password = accounts[accountId].password;
 
         require(balance > 0, "No pending transfers");
-        require(password == sha256(exchangePassword, recipientPassword), "Incorrect passwords");
+        require(password == createPassword(exchangePassword, recipientPassword),
+                "Incorrect passwords");
 
         // update state before sending funds to prevent reentry attacks
         delete accounts[accountId];
@@ -65,4 +66,15 @@ contract Remittance {
         require(msg.sender.send(balance), "Failed to send funds");
     }
 
+    function createPassword(string part1, string part2) public pure returns(bytes32) {
+        return sha256(abi.encodePacked(part1, part2));
+    }
+
+    function getAccountBalance(uint accountId) public view returns(uint) {
+        return accounts[accountId].balance;
+    }
+
+    function getAccountPassword(uint accountId) public view returns(bytes32) {
+        return accounts[accountId].password;
+    }
 }
--- test/remittance.js
+++ test/remittance.js
@@ -1,5 +1,63 @@
-var MetaCoin = artifacts.require("./Remittance.sol");
+var Remittance = artifacts.require("./Remittance.sol");
 
 contract('Remittance', function(accounts) {
+
+  it("deposit should create a new account", function() {
+        var meta;
+
+        var account_one = accounts[0];
+
+        var accountId = 1;
+        var accountBalanceBefore;
+        var accountBalanceAfter;
+        var amount = 100;
+        var password =  "secret";
+
+        return Remittance.deployed().then(function(instance) {
+          meta = instance;
+          return meta.deposit(accountId, web3.fromUtf8(password),
+                { from:account_one, value: amount, gas: 10000000,  gasPrice: 1 });
+        }).then(function() {
+          return meta.getAccountBalance.call(accountId);
+        }).then(function(balance) {
+          accountBalanceAfter = balance.toNumber();
+        }).then(function() {
+            assert.equal(amount, accountBalanceAfter,
+              "account balance is iccorrect");
+        });
+  });
+
+  it("withdraw should delete account", function() {
+        var meta;
+
+        var account_one = accounts[0];
+        var account_two = accounts[1];
+
+        var accountId = 2;
+        var accountEndingBalance;
+        var amount = 100;
+        var passwordPart1 = "top";
+        var passwordPart2 = "secret";
+        var password;
+
+        return Remittance.deployed().then(function(instance) {
+          meta = instance;
+          return meta.createPassword.call(passwordPart1, passwordPart2);
+        }).then(function(pwd) {
+          password = pwd;
+          return meta.deposit(accountId, password,
+                { from:account_one, value: amount, gas: 10000000,  gasPrice: 1 });
+        }).then(function() {
+          return meta.withdraw(accountId, passwordPart1, passwordPart2,
+                { from:account_two, gas: 10000000,  gasPrice: 1 });
+        }).then(function() {
+          return meta.getAccountBalance.call(accountId);
+        }).then(function(balance) {
+          accountEndingBalance = balance.toNumber();
+        }).then(function() {
+          assert.equal(0, accountEndingBalance,
+            "account balance is iccorrect after withdraw");
+        });
+  });
 
 });
